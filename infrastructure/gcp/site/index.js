54c6c7270120f1e9c52b1bb36122598fc22d84af
Matthew Green <matthewgr@nandos.co.uk>
2020-08-18 13:20:13 +0100

    Removed artefacts bucket as I don't think it will be needed yet

--- infrastructure/gcp/site/index.js
+++ infrastructure/gcp/site/index.js
@@ -4,9 +4,6 @@ const gcp = require("@pulumi/gcp");
 
 const name = pulumi.getProject();
 
-// Create a GCP resource (Storage Bucket For build artifacts)
-const artefactBucket = new gcp.storage.Bucket("artefacts");
-
 // Create a GCP resource (Storage Bucket For website)
 const websiteBucket = new gcp.storage.Bucket(name + "-website");
 const websiteBackend = new gcp.compute.BackendBucket(name + "-website-backend",
@@ -16,11 +13,6 @@ const websiteBackend = new gcp.compute.BackendBucket(name + "-website-backend",
   enableCdn: true,
 });
 
-// Create a GCP resource (Cloud Build for website)
-
-
 // Export the DNS name of the bucket
 exports.websiteBucketName = websiteBucket.url;
-exports.websiteBAckend = websiteBackend.urn;
-// Export the DNS name of the artefact bucket
-exports.ArtifactsBucketName = artefactBucket.url;
+exports.websiteBackend = websiteBackend.urn;
