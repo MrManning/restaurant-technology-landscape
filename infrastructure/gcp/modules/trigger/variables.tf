e892c574c2312d7828fc2410d9b96fb0217fc228
Kyle Manning <kyle.manning@nandos.co.uk>
2020-12-21 13:19:11 +0000

    Add trigger variables and output variables

--- infrastructure/gcp/modules/trigger/variables.tf
+++ infrastructure/gcp/modules/trigger/variables.tf
@@ -1,13 +1,14 @@
 variable "branch" {}
 variable "cloudbuild" {}
 variable "description" {}
-variable "name" {}
-variable "owner" {}
-variable "project_id" {}
 variable "include" {
   type    = list(string)
   default = []
 }
+variable "name" {}
+variable "owner" {}
+variable "project_id" {}
+variable "repository" {}
 variable "substitutions" {
   type = map(string)
 }
